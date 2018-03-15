05cdf45a2eb9a034362e7bec2b5a82eeddfc9396
Fran Rúa <fran.ruab@gmail.com>
2018-03-15 20:24:55 +0100

    Actividad ajustes, spinner de sonido inicial

--- app/src/main/java/com/sedentapp/sedentapp/sedentapp/Ajustes.java
+++ app/src/main/java/com/sedentapp/sedentapp/sedentapp/Ajustes.java
@@ -6,8 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Ajustes extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ajustes extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,37 @@ public class Ajustes extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Spinner sound_spinner = (Spinner) findViewById(R.id.sound_spinner);
+
+        sound_spinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Fresh.mp3");
+        categories.add("Superhit.mp3");
+        categories.add("Song2");
+
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+      sound_spinner.setAdapter(dataAdapter);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+     public void onNothingSelected(AdapterView<?> arg0) {
+      // TODO Auto-generated method stub
+   }
 
 }
