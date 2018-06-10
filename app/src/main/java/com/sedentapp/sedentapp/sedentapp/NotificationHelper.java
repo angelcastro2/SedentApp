b19a69f549394050b9637b9383b78ca97019ed06
Angel Castro <ancasya@gmail.com>
2018-06-10 21:59:05 +0200

    mas funcionalidad en notificaciones

--- app/src/main/java/com/sedentapp/sedentapp/sedentapp/NotificationHelper.java
+++ app/src/main/java/com/sedentapp/sedentapp/sedentapp/NotificationHelper.java
@@ -27,41 +27,45 @@ public class NotificationHelper {
      */
     public void createNotification(String title, String message, Context context)
     {
-        /**Creates an explicit intent for an Activity in your app**/
-        Intent resultIntent = new Intent(mContext , MainActivity.class);
-        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean notificacionPrefActivas = sharedPref.getBoolean("notifications_new_message", true);
+        if(notificacionPrefActivas){
+            /**Creates an explicit intent for an Activity in your app**/
+            Intent resultIntent = new Intent(mContext , MainActivity.class);
+            resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(mContext,
-                0 /* Request code */, resultIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent resultPendingIntent = PendingIntent.getActivity(mContext,
+                    0 /* Request code */, resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
 
-        mBuilder = new NotificationCompat.Builder(mContext);
-        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
-        mBuilder.setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(false)
-                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
-                .setContentIntent(resultPendingIntent);
+            mBuilder = new NotificationCompat.Builder(mContext);
+            mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+            mBuilder.setContentTitle(title)
+                    .setContentText(message)
+                    .setAutoCancel(false)
+                    .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                    .setContentIntent(resultPendingIntent);
 
-        mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
-        {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.GREEN);
-            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
-            boolean notificacionPref = sharedPref.getBoolean("notifications_new_message_vibrate", true);
-            if(notificacionPref){
-                notificationChannel.enableVibration(true);
-                notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+            {
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.GREEN);
+                boolean notificacionPrefvibrate = sharedPref.getBoolean("notifications_new_message_vibrate", true);
+                if(notificacionPrefvibrate){
+                    notificationChannel.enableVibration(true);
+                    notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                }
+                assert mNotificationManager != null;
+                mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
+                mNotificationManager.createNotificationChannel(notificationChannel);
             }
             assert mNotificationManager != null;
-            mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
-            mNotificationManager.createNotificationChannel(notificationChannel);
+            mNotificationManager.notify(0 /* Request Code */, mBuilder.build());
         }
-        assert mNotificationManager != null;
-        mNotificationManager.notify(0 /* Request Code */, mBuilder.build());
+
     }
 }
