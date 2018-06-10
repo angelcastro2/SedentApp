e91b0724783df65d821d97e802ded1a2cdfb4ec3
Kike <kgoberna@gmail.com>
2018-06-10 21:06:34 +0200

    Añadida cláusa ORDER BY a las consultas SQL

--- app/src/main/java/com/sedentapp/sedentapp/sedentapp/entities/registropasos/dao/RegistroPasosDao.java
+++ app/src/main/java/com/sedentapp/sedentapp/sedentapp/entities/registropasos/dao/RegistroPasosDao.java
@@ -17,22 +17,22 @@ public interface RegistroPasosDao {
     @Insert
     void save(RegistroPasos registroPasos);
 
-    @Query("SELECT * FROM RegistroPasos WHERE registroPasosId = :registroPasosId")
+    @Query("SELECT * FROM RegistroPasos WHERE registroPasosId = :registroPasosId ORDER BY hora,dia,mes,ano")
     RegistroPasos find(long registroPasosId);
 
-    @Query("SELECT * FROM RegistroPasos WHERE dia = :dia AND mes = :mes AND ano = :ano AND hora = :hora")
+    @Query("SELECT * FROM RegistroPasos WHERE dia = :dia AND mes = :mes AND ano = :ano AND hora = :hora ORDER BY hora,dia,mes,ano")
     RegistroPasos getPasosByFechaAndHora(int dia, int mes, int ano, int hora);
 
     //@Query("SELECT SUM(pasos) FROM RegistroPasos WHERE dia = :dia AND mes = :mes AND ano = :ano")
    // RegistroPasos getPasosByDia(int dia, int mes, int ano);
 
-    @Query("SELECT * FROM RegistroPasos WHERE dia = :dia AND mes = :mes AND ano = :ano")
+    @Query("SELECT * FROM RegistroPasos WHERE dia = :dia AND mes = :mes AND ano = :ano ORDER BY hora,dia,mes,ano")
     List<RegistroPasos> getRegistroPasosByDia(int dia, int mes, int ano);
 
-    @Query("SELECT * FROM RegistroPasos WHERE mes = :mes AND ano = :ano")
+    @Query("SELECT * FROM RegistroPasos WHERE mes = :mes AND ano = :ano ORDER BY hora,dia,mes,ano")
     List<RegistroPasos> getRegistroPasosByMes(int mes, int ano);
 
-    @Query("SELECT * FROM RegistroPasos WHERE ano = :ano")
+    @Query("SELECT * FROM RegistroPasos WHERE ano = :ano ORDER BY hora,dia,mes,ano")
     List<RegistroPasos> getRegistroPasosByAno(int ano);
 
     @Update
