3a6bc6693b34d729b3e4a54e6069acd67305519d
Kike <kgoberna@gmail.com>
2018-06-10 21:22:36 +0200

    Actualizado tiempo máximo de inactividad según las preferencias del usuario

--- app/src/main/java/com/sedentapp/sedentapp/sedentapp/CheckInactivityTask.java
+++ app/src/main/java/com/sedentapp/sedentapp/sedentapp/CheckInactivityTask.java
@@ -5,7 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.provider.CalendarContract;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -22,25 +24,21 @@ public class CheckInactivityTask extends TimerTask {
 
     private RegistroPasosService registroPasosService;
     private Context context;
+    private SharedPreferences sharedPref;
 
     private NotificationManager mNotificationManager;
 
     private final String TAG = "SedentApp";
 
-    public CheckInactivityTask(Context context, NotificationManager mNotificationManager ) {
+    public CheckInactivityTask(Context context) {
         Log.d(TAG, "[CheckInactivityTask] Constructor");
         this.registroPasosService = new RegistroPasosService();
         this.context = context;
         this.mNotificationManager = mNotificationManager;
+        this.sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     private int getInactivityHours(List<RegistroPasos> registroPasosByDia) {
-
-        int hora1 = 0;
-        int hora2 = 0;
-
-        int max_diff = 0;
-        int diff = 0;
 
         if (registroPasosByDia.size() > 0) {
             int lastHour = registroPasosByDia.get(registroPasosByDia.size()-1).getHora();
@@ -73,22 +71,9 @@ public class CheckInactivityTask extends TimerTask {
         intent.setAction("com.sedentapp.update_inactivity_time_counter");
         this.context.sendBroadcast(intent);
 
+        int tiempoLimite = sharedPref.getInt("inactividad_list", 9999);
 
-//        Intent resultIntent = new Intent(this.context, MainActivity.class);
-//        PendingIntent pIntent = PendingIntent.getActivity(this.context, 0, resultIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
-
-//        Notification.Builder mBuilder = new Notification.Builder(this.context) // builder notification
-//                .setSmallIcon(R.drawable.ic_fitness_center_black_24dp)
-//                .setContentTitle("Tiempo de inactividad demasiado alto!")
-//                .setContentText("Llevas " + inactivityHours + " horas inactivo") .setWhen(System.currentTimeMillis())
-//                .setVibrate(new long[]{0,100,200,300}).setLights(Color.RED, 2000, 1000)
-//                .setTicker("New notification arrived!") // text shown when notification arrived
-//                .addAction(android.R.drawable.ic_menu_share, "Share", pIntent) // max. 3 buttons
-//                .setContentIntent(pIntent);
-//        mNotificationManager.notify(0, mBuilder.build());
-
-
-        if (inactivityHours > 2) {
+        if ((tiempoLimite != -1) & (inactivityHours > tiempoLimite)) {
             NotificationHelper notificationHelper = new NotificationHelper(this.context);
             notificationHelper.createNotification("Tiempo de inactividad demasiado alto!","Llevas " + inactivityHours + " horas inactivo");
         }
