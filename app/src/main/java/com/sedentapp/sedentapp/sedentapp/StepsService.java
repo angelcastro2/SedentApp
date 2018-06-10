7a8543e39d6407e57ab9c370f10c8afc3e29bdcb
Kike <kgoberna@gmail.com>
2018-06-10 13:36:17 +0200

    Añadido registro en base de datos de los pasos detectados con el sensor

--- app/src/main/java/com/sedentapp/sedentapp/sedentapp/StepsService.java
+++ app/src/main/java/com/sedentapp/sedentapp/sedentapp/StepsService.java
@@ -12,26 +12,44 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.sedentapp.sedentapp.sedentapp.entities.registropasos.RegistroPasos;
+import com.sedentapp.sedentapp.sedentapp.entities.registropasos.service.RegistroPasosService;
+
+import java.util.Calendar;
+
+import static android.content.pm.PackageManager.FEATURE_SENSOR_STEP_COUNTER;
+import static android.content.pm.PackageManager.FEATURE_SENSOR_STEP_DETECTOR;
+
 public class StepsService extends Service implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mStepDetectorSensor;
     //private StepsDBHelper mStepsDBHelper;
     protected static final String TAG = "SedentApp";
-
+    private RegistroPasosService registroPasosService;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "[StepsService] onCreate");
+
         mSensorManager = (SensorManager)
                 this.getSystemService(Context.SENSOR_SERVICE);
+
         if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null) {
             mStepDetectorSensor =
                     mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
             mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
             //mStepsDBHelper = new StepsDBHelper(this);
+            Log.d(TAG, "[StepsService] Step detector sensor registered");
+
+            this.registroPasosService = new RegistroPasosService();
+
         }
+        else {
+            Log.e(TAG, "[StepsService] Cannot register step detector sensor");
+        }
+
     }
 
     @Override
@@ -49,8 +67,46 @@ public class StepsService extends Service implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.d(TAG, "[StepsService] STEP DETECTED");
-        //aqui guardar pasos en la base de datos
+        Log.d(TAG, "[StepsService] onSensorChanged");
+
+        String sensorName = event.sensor.getName();
+        Log.d(TAG, sensorName + ": X: " + event.values[0] + "; Y: " + event.values[1] + "; Z: " + event.values[2] + ";");
+
+        if (sensorName.equals(this.mStepDetectorSensor.getName()) | event.sensor.equals(this.mStepDetectorSensor)) {
+            Log.d(TAG, "[StepsService] STEP DETECTED!");
+            int steps = (int) event.values[0];
+            updateStepCounter(steps);
+
+        }
+
+    }
+
+    private void updateStepCounter(int steps) {
+
+        Log.d(TAG, "[StepsService] updateStepCounter");
+
+        Calendar calendar = Calendar.getInstance();
+
+        RegistroPasos registroPasos = this.registroPasosService.getRegistroPasosByFechaAndHora(this,
+                calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.HOUR_OF_DAY));
+
+        if (registroPasos != null) {
+            registroPasos.setPasos(registroPasos.getPasos() + steps);
+            this.registroPasosService.update(registroPasos, this);
+        }
+        else {
+            registroPasos = new RegistroPasos(calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.YEAR), calendar.get(Calendar.HOUR_OF_DAY), steps);
+            this.registroPasosService.save(registroPasos, this);
+        }
+
+        Log.d(TAG, "[StepsService] StepRegister at " + calendar.toString() + ": " + registroPasos.getPasos());
+
+        Intent intent = new Intent();
+        intent.setAction("com.sedentapp.read_daily_step_counter");
+        sendBroadcast(intent);
+
     }
 
     @Override
